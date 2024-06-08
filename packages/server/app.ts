4a1862051f0153e284c22d4c66c2e71e3a98f253
billi.tsx <sebastian.quispe.b@utec.edu.pe>
2024-06-08 00:29:04 -0500

    feat: added cors and logger to server

--- packages/server/app.ts
+++ packages/server/app.ts
@@ -1,10 +1,18 @@
+import cors from "cors";
 import express from "express";
+import morgan from "morgan";
 import ordersRoutes from "./routes/orders";
 import productsRoutes from "./routes/products";
 
 const app = express();
+const corsOptions = {
+  origin: "http://localhost:5173",
+};
 
+app.use(morgan("combined"));
 app.use(express.json());
+app.use(cors(corsOptions));
+
 app.use("/api", ordersRoutes);
 app.use("/api", productsRoutes);
 
